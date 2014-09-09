d29ffff0fa25c0a737c64ca29f114acbf0ac8fbf
aynroot <valeriyachernenko@gmail.com>
2014-09-10 02:01:46 +0400

    improved acc to 0.56 by using factor variables instead of integer

--- models.R
+++ models.R
@@ -12,8 +12,18 @@ test <- test[, -1]
 train$Cover_Type <- as.factor(train$Cover_Type)
 test$Cover_Type <- as.factor(test$Cover_Type)
 
-make_submission(model) {
+make_submission(model, filename) {
+    test_data <- read.csv("test.csv")
+    test_data$Cover_Type <- rep(0, dim(test_data)[1])
+    submission_data <- subset(test_data, select=c("Id", "Cover_Type"))
+    test_data <- test_data[, -1]
 
+    # add preprocessing
+    test_data$Cover_Type <- as.integer(predict(model, newdata=test_data))
+    submission_data$Cover_Type <- test_data$Cover_Type
+
+    # save to csv file
+    write.csv(file=filename, x=submission_data, row.names=FALSE)
 }
 
 evaluate(model, test_data) {
@@ -26,5 +36,6 @@ evaluate(model, test_data) {
 
 kNNfit <- function(train, test) {
     ctrl <- trainControl(method="cv", number = 7)
-    knnFit <- train(Cover_Type ~ ., data=train, method = "knn", trControl = ctrl, preProcess = c("center","scale"), tuneLength = 10)
+    model <- train(Cover_Type ~ ., data=train, method = "knn", trControl = ctrl, preProcess = c("center","scale"), tuneLength = 10)
+    model
 }
